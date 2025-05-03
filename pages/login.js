fb102dd3059436543e0aa9b01bf5f1a627086468
daffamusyafa <121594058+daffamusyafa@users.noreply.github.com>
2025-05-03 21:50:48 +0700

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Navbar from "../components/Navbar";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+    if (res.ok) {
+      localStorage.setItem("token", data.token);
+      alert("Login sukses");
+      router.push("/");
+    } else {
+      alert(data.error || "Login gagal");
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        /><br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        /><br />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+            }
